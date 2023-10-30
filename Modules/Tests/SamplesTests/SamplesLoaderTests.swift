c838ef6e6d7087c060a18ad68e8dc925ca1ac3c2
Max Gribov <gribov.max@gmail.com>
2023-10-30 19:38:30 +0300

    init does not request any data

--- Modules/Tests/SamplesTests/SamplesLoaderTests.swift
+++ Modules/Tests/SamplesTests/SamplesLoaderTests.swift
@@ -7,29 +7,27 @@
 
 import XCTest
 
+final class SamplesLoader {
+    
+    init(store: SamplesLoaderTests.SamplesStoreSpy) {
+        
+    }
+}
+
 final class SamplesLoaderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotRequestData() {
+        
+        let store = SamplesStoreSpy()
+        let _ = SamplesLoader(store: store)
+        
+        XCTAssertEqual(store.receivedRequests.count, 0)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    //MARK: - Helpers
+    
+    final class SamplesStoreSpy {
+        
+        var receivedRequests = [Any]()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
