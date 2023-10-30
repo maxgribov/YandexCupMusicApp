87c478c840921dc9ad1ea47862a1f0198a79c6d7
Max Gribov <gribov.max@gmail.com>
2023-10-30 20:24:29 +0300

    load delivers samples on success retrieval

--- Modules/Tests/SamplesTests/SamplesLoaderTests.swift
+++ Modules/Tests/SamplesTests/SamplesLoaderTests.swift
@@ -16,7 +16,7 @@ final class SamplesLoader {
         self.store = store
     }
     
-    func load(for instrument: Instrument, completion: @escaping (Result<Void, Error>) -> Void) {
+    func load(for instrument: Instrument, completion: @escaping (Result<[Sample], Error>) -> Void) {
         
         store.retrieveSamples(for: instrument, completion: completion)
     }
@@ -27,6 +27,12 @@ enum Instrument {
     case guitar
     case drums
     case tube
+}
+
+struct Sample: Equatable {
+    
+    let name: String
+    let data: Data
 }
 
 final class SamplesLoaderTests: XCTestCase {
@@ -51,7 +57,7 @@ final class SamplesLoaderTests: XCTestCase {
         
         let (sut, store) = makeSUT()
         
-        var receivedError: Error?
+        var receivedError: Error? = nil
         sut.load(for: .guitar) { result in
             switch result {
             case let .failure(error):
@@ -65,6 +71,26 @@ final class SamplesLoaderTests: XCTestCase {
         store.complete(with: anyNSError())
         
         XCTAssertEqual(receivedError as? NSError, anyNSError())
+    }
+    
+    func test_load_deliversSamplesOnSuccessRetrieval() {
+        
+        let (sut, store) = makeSUT()
+        
+        var receivedSamples: [Sample]? = nil
+        sut.load(for: .drums) { result in
+            switch result {
+            case let .success(samples):
+                receivedSamples = samples
+                
+            default:
+                break
+            }
+        }
+        
+        store.complete(with: uniqueSamples())
+        
+        XCTAssertEqual(receivedSamples, uniqueSamples())
     }
     
     //MARK: - Helpers
@@ -83,22 +109,31 @@ final class SamplesLoaderTests: XCTestCase {
             case retrieveSamplesFor(Instrument)
         }
         
-        private var completions = [(request: Request, completion: (Result<Void, Error>) -> Void)]()
+        private var completions = [(request: Request, completion: (Result<[Sample], Error>) -> Void)]()
         
         var receivedRequests: [Request] {
             completions.map(\.request)
         }
         
-        func retrieveSamples(for instrument: Instrument, completion: @escaping (Result<Void, Error>) -> Void) {
+        func retrieveSamples(for instrument: Instrument, completion: @escaping (Result<[Sample], Error>) -> Void) {
             completions.append((.retrieveSamplesFor(instrument), completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
             completions[index].completion(.failure(error))
         }
+        
+        func complete(with samples: [Sample], at index: Int = 0) {
+            completions[index].completion(.success(samples))
+        }
     }
     
     private func anyNSError() -> NSError {
         .init(domain: "", code: 0)
+    }
+    
+    private func uniqueSamples() -> [Sample] {
+        [.init(name: "first", data: Data("first-data".utf8)),
+         .init(name: "second", data: Data("second-data".utf8))]
     }
 }
