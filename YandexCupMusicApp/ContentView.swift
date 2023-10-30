cf9f8406a478e71dd1e0b9de2526bebc834c733e
Max Gribov <gribov.max@gmail.com>
2023-10-30 19:01:51 +0300

    Initial Commit

---
+++ YandexCupMusicApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YandexCupMusicApp
+//
+//  Created by Max Gribov on 30.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YandexCupMusicApp/YandexCupMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexCupMusicAppApp.swift
+//  YandexCupMusicApp
+//
+//  Created by Max Gribov on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct YandexCupMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
