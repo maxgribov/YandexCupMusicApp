d8a1b45c164e8151d84ba597328beab061902dbe
Max Gribov <gribov.max@gmail.com>
2023-10-31 18:10:45 +0300

    addLayerForRecording adds layer with correct properties an incremented number

--- Modules/Tests/ProducerTests/ProducerTests.swift
+++ Modules/Tests/ProducerTests/ProducerTests.swift
@@ -28,11 +28,26 @@ final class Producer {
         layers.append(layer)
     }
     
+    func addLayer(id: UUID = UUID(), forRecording data: Data) {
+        
+        let layer = Layer(id: id, name: recordingLayerName(), isPlaying: false, isMuted: false, control: .initial)
+        payloads[layer.id] = .recording(data)
+        layers.append(layer)
+    }
+    
     private func sampleLayerName(for instrument: Instrument) -> String {
         
         let currentLayersCount = payloads.values.compactMap(\.instrument).filter { $0 == instrument }.count
         
         return "\(instrument.name) \(currentLayersCount + 1)"
+    }
+    
+    private func recordingLayerName() -> String {
+        
+        let currentLayersCount = payloads.values.map(\.isRecording).filter { $0 }.count
+        
+        //TODO: localisation required
+        return "Запись \(currentLayersCount + 1)"
     }
 }
 
@@ -50,6 +65,14 @@ extension Producer {
             }
             
             return instrument
+        }
+        
+        var isRecording: Bool {
+            
+            switch self {
+            case .recording: return true
+            default: return false
+            }
         }
     }
 }
@@ -88,6 +111,24 @@ final class ProducerTests: XCTestCase {
                                     .init(id: thirdLayerID, name: "Ударные 1", isPlaying: false, isMuted: false, control: .initial)])
     }
     
+    func test_addLayerForRecording_addsLayerWithCorrectPropertiesAndIncrementingNumber() {
+        
+        let (sut, _) = makeSUT()
+        
+        let firstLayerID = UUID()
+        sut.addLayer(id: firstLayerID, forRecording: someRecordingData())
+        
+        let secondLayerID = UUID()
+        sut.addLayer(id: secondLayerID, forRecording: someRecordingData())
+        
+        let thirdLayerID = UUID()
+        sut.addLayer(id: thirdLayerID, forRecording: someRecordingData())
+        
+        XCTAssertEqual(sut.layers, [.init(id: firstLayerID, name: "Запись 1", isPlaying: false, isMuted: false, control: .initial),
+                                    .init(id: secondLayerID, name: "Запись 2", isPlaying: false, isMuted: false, control: .initial),
+                                    .init(id: thirdLayerID, name: "Запись 3", isPlaying: false, isMuted: false, control: .initial)])
+    }
+    
 
     
     private func makeSUT() -> (sut: Producer, player: PlayerSpy) {
@@ -105,6 +146,10 @@ final class ProducerTests: XCTestCase {
     
     private func someSample() -> Sample {
         .init(id: UUID().uuidString, data: Data(UUID().uuidString.utf8))
+    }
+    
+    private func someRecordingData() -> Data {
+        Data(UUID().uuidString.utf8)
     }
     
 }
