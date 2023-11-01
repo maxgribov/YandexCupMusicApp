63ebb3fecc4ceeff1720531b3bd1a6811126c4a7
Max Gribov <gribov.max@gmail.com>
2023-11-01 15:01:11 +0300

    play invokes play on AVAudioPlayer

--- Modules/Tests/ProcessingTests/AVFoundationPlayerTests.swift
+++ Modules/Tests/ProcessingTests/AVFoundationPlayerTests.swift
@@ -13,26 +13,40 @@ import AVFoundation
 protocol AVAudioPlayerProtocol {
     
     init(data: Data) throws
+    @discardableResult
+    func play() -> Bool
 }
 
-final class AVFoundationPlayer<P: AVAudioPlayerProtocol> {
+final class AVFoundationPlayer {
     
     private(set) var playing: Set<Layer.ID>
+    private let makePlayer: (Data) throws -> AVAudioPlayerProtocol
     
-    init() {
+    init(makePlayer: @escaping (Data) throws -> AVAudioPlayerProtocol) {
         
         self.playing = []
+        self.makePlayer = makePlayer
     }
     
     func play(id: Layer.ID, data: Data, control: Layer.Control) {
         
-        guard let player = try? P(data: data) else {
+        guard let player = try? makePlayer(data) else {
             return
         }
+        
+        player.play()
     }
 }
 
 final class AVFoundationPlayerTests: XCTestCase {
+    
+    var player: AVAudioPlayerSpy? = nil
+    
+    override func setUp() async throws {
+        try await super.setUp()
+        
+        player = nil
+    }
     
     func test_init_nothingPlaying() {
         
@@ -43,9 +57,22 @@ final class AVFoundationPlayerTests: XCTestCase {
     
     func test_play_nothingPlayingOnPlayerInitFailure() {
         
-        let sut = AVFoundationPlayer<AlwaysFailingAVAudioPlayerStub>()
+        let sut = AVFoundationPlayer { data in
+            try AlwaysFailingAVAudioPlayerStub(data: data)
+        }
+        
+        sut.play(id: anyLayerID(), data: anyData(), control: .initial)
         
         XCTAssertTrue(sut.playing.isEmpty)
+    }
+    
+    func test_play_invokesPlayOnPlayerSuccessfulInit() {
+        
+        let sut = makeSUT()
+        
+        sut.play(id: anyLayerID(), data: anyData(), control: .initial)
+        
+        XCTAssertEqual(self.player?.messages, [.initWithData, .play])
     }
     
     //MARK: - Helpers
@@ -53,27 +80,54 @@ final class AVFoundationPlayerTests: XCTestCase {
     private func makeSUT(
         file: StaticString = #filePath,
         line: UInt = #line
-    ) -> AVFoundationPlayer<AVAudioPlayerSpy> {
+    ) -> AVFoundationPlayer {
         
-        let sut = AVFoundationPlayer<AVAudioPlayerSpy>()
-        trackForMemoryLeaks(sut)
+        let sut = AVFoundationPlayer {[weak self] data in
+            let _player = try AVAudioPlayerSpy(data: data)
+            self?.player = _player
+            return _player
+        }
+        trackForMemoryLeaks(sut, file: file, line: line)
         
         return sut
     }
     
     class AVAudioPlayerSpy: AVAudioPlayerProtocol {
         
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            
+            case initWithData
+            case play
+        }
+        
         required init(data: Data) throws {
-            throw NSError(domain: "", code: 0)
+            
+            messages.append(.initWithData)
+        }
+        
+        @discardableResult
+        func play() -> Bool {
+            
+            messages.append(.play)
+            return true
         }
     }
     
     class AlwaysFailingAVAudioPlayerStub: AVAudioPlayerProtocol {
         
         required init(data: Data) throws {
+            
             throw NSError(domain: "", code: 0)
         }
+        
+        @discardableResult
+        func play() -> Bool { false }
     }
+    
+    private func anyLayerID() -> Layer.ID { UUID() }
+    private func anyData() -> Data { Data(UUID().uuidString.utf8) }
 }
 
 extension AVAudioPlayer: AVAudioPlayerProtocol {}
