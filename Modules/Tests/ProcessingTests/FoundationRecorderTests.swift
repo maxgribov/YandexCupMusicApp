fa8797759e2589da99493b3786dbdec6998297d9
Max Gribov <gribov.max@gmail.com>
2023-11-01 20:13:00 +0300

    startRecording set category for session on first attempt

--- Modules/Tests/ProcessingTests/FoundationRecorderTests.swift
+++ Modules/Tests/ProcessingTests/FoundationRecorderTests.swift
@@ -10,7 +10,7 @@ import AVFoundation
 import Combine
 
 #if os(iOS)
-protocol AVAudioSessionProtocol {
+protocol AVAudioSessionProtocol: AnyObject {
     
     func setCategory(
         _ category: AVAudioSession.Category,
@@ -24,23 +24,108 @@ extension AVAudioSession: AVAudioSessionProtocol {}
 final class FoundationRecorder {
     
     private let isRecordingSubject = CurrentValueSubject<Bool, Never>(false)
+    private let session: AVAudioSessionProtocol
+    private var permissionsState: Permissions
+    
+    init(session: AVAudioSessionProtocol) {
+        
+        self.session = session
+        self.permissionsState = .required
+    }
     
     func isRecording() -> AnyPublisher<Bool, Never> {
         
         isRecordingSubject.eraseToAnyPublisher()
     }
+    
+    func startRecording() throws -> AnyPublisher<Data, Error> {
+        
+        switch permissionsState {
+        case .required:
+            return configureSessionAndRequestPermissions()
+                .handleEvents(receiveOutput: { self.permissionsState = $0 ? .allowed : .rejected })
+                .flatMap { result in
+                    
+                    return Future<Data, Error> { promise in
+                        
+                        promise(.success(Data()))
+                    }
+                    
+                }.eraseToAnyPublisher()
+            
+        case .allowed:
+            return Future<Data, Error> { promise in
+                
+                promise(.success(Data()))
+                
+            }.eraseToAnyPublisher()
+            
+        case .rejected:
+            return Fail<Data, Error>(error: NSError(domain: "", code: 0)).eraseToAnyPublisher()
+        }
+    }
+    
+    private func configureSessionAndRequestPermissions() -> AnyPublisher<Bool, Error> {
+        
+        Future { [weak self] promise in
+            
+            do {
+                
+                try self?.session.setCategory(.playAndRecord, mode: .default, options: [])
+                promise(.success(true))
+                
+            } catch {
+                
+                promise(.failure(error))
+            }
+            
+        }.eraseToAnyPublisher()
+    }
+    
+    enum Permissions {
+        
+        case required
+        case allowed
+        case rejected
+    }
 }
+
 
 final class FoundationRecorderTests: XCTestCase {
 
     func test_init_recordingNothing() {
         
-        let sut = FoundationRecorder()
+        let session = AVAudioSessionSpy()
+        let sut = FoundationRecorder(session: session)
         let isRecordingSpy = ValueSpy(sut.isRecording())
         
         XCTAssertEqual(isRecordingSpy.values, [false])
     }
-
+    
+    func test_startRecording_setCategoryForSessionOnFirstAttempt() throws {
+        
+        let session = AVAudioSessionSpy()
+        let sut = FoundationRecorder(session: session)
+        
+        _ = try sut.startRecording()
+        
+        XCTAssertEqual(session.messages, [.setCategory(.playAndRecord, .default)])
+    }
+    
+    private class AVAudioSessionSpy: AVAudioSessionProtocol {
+        
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            
+            case setCategory(AVAudioSession.Category, AVAudioSession.Mode)
+        }
+        
+        func setCategory(_ category: AVAudioSession.Category, mode: AVAudioSession.Mode, options: AVAudioSession.CategoryOptions = []) throws {
+            
+            messages.append(.setCategory(category, mode))
+        }
+    }
 }
 
 #endif
