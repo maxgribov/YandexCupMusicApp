e3f5c71eebe7fb06d68dcea3d58ae2f67b8b15dc
Max Gribov <gribov.max@gmail.com>
2023-11-02 15:28:11 +0300

    control has value on initial update value

--- Modules/Tests/PresentationTests/SampleControlViewModelTests.swift
+++ Modules/Tests/PresentationTests/SampleControlViewModelTests.swift
@@ -9,18 +9,33 @@ import XCTest
 import Combine
 import Domain
 
-struct SampleControlViewModel {
+final class SampleControlViewModel {
     
-    private(set) var control: Layer.Control?
+    @Published private(set) var control: Layer.Control?
+    
+    init(update: AnyPublisher<Layer.Control?, Never>) {
+        
+        self.control = nil
+        update.assign(to: &$control)
+    }
 }
 
 final class SampleControlViewModelTests: XCTestCase {
-
-    func test_init_controlNil() {
+    
+    func test_init_controlNilWithUpdateNil() {
         
-        let sut = SampleControlViewModel()
+        let sut = SampleControlViewModel(update: Just(nil).eraseToAnyPublisher())
+        let controlValueSpy = ValueSpy(sut.$control)
         
-        XCTAssertNil(sut.control)
+        XCTAssertEqual(controlValueSpy.values, [nil])
+    }
+    
+    func test_init_controlValueWithUpdateValue() {
+        
+        let sut = SampleControlViewModel(update: Just(.init(volume: 1, speed: 1)).eraseToAnyPublisher())
+        let controlValueSpy = ValueSpy(sut.$control)
+        
+        XCTAssertEqual(controlValueSpy.values, [.init(volume: 1, speed: 1)])
     }
 
 }
