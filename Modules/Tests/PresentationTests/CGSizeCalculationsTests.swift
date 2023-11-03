ce30665c7dab6a4e6c59774d5b3ccc653f82bf91
Max Gribov <gribov.max@gmail.com>
2023-11-03 11:12:51 +0300

    CGSize limit area expected results

--- Modules/Tests/PresentationTests/CGSizeCalculationsTests.swift
+++ Modules/Tests/PresentationTests/CGSizeCalculationsTests.swift
@@ -19,10 +19,13 @@ extension CGSize {
     
     func limit(area: CGSize) -> CGSize {
         
-        let centerX = area.width / 2
-        let centerY = area.height / 2
+        let halfAreaWidth = abs(area.width / 2)
+        let halfAreaHeight = abs(area.height / 2)
         
-        return self
+        let updatedWidth = min(abs(width), halfAreaWidth) * (width.sign == .minus ? -1 : 1)
+        let updatedHeight = min(abs(height), halfAreaHeight) * (height.sign == .minus ? -1 : 1)
+        
+        return .init(width: updatedWidth, height: updatedHeight)
     }
 }
 
@@ -35,5 +38,15 @@ final class CGSizeCalculationsTests: XCTestCase {
         XCTAssertEqual(CGSize.zero.offset(translation: .init(width: -100, height: -200)), .init(width: -100, height: -200))
         XCTAssertEqual(CGSize(width: 100, height: 100).offset(translation: .init(width: 50, height: 50)), .init(width: 150, height: 150))
         XCTAssertEqual(CGSize(width: 100, height: 100).offset(translation: .init(width: -50, height: -50)), .init(width: 50, height: 50))
+    }
+    
+    func test_limit_expectedResults() {
+        
+        XCTAssertEqual(CGSize.zero.limit(area: .zero), .zero)
+        XCTAssertEqual(CGSize.zero.limit(area: .init(width: 100, height: 100)), .zero)
+        XCTAssertEqual(CGSize(width: 50, height: 50).limit(area: .init(width: 100, height: 100)), .init(width: 50, height: 50))
+        XCTAssertEqual(CGSize(width: 100, height: 100).limit(area: .init(width: 100, height: 100)), .init(width: 50, height: 50))
+        XCTAssertEqual(CGSize(width: -50, height: -50).limit(area: .init(width: 100, height: 100)), .init(width: -50, height: -50))
+        XCTAssertEqual(CGSize(width: -100, height: -100).limit(area: .init(width: 100, height: 100)), .init(width: -50, height: -50))
     }
 }
