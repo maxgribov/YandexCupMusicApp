a17926b18c7d86a969cae53e4b84384ae451ff0d
Max Gribov <gribov.max@gmail.com>
2023-11-03 18:35:13 +0300

    init controlPanel contains correct buttons

--- YandexCupMusicAppTests/MainViewModelTests.swift
+++ YandexCupMusicAppTests/MainViewModelTests.swift
@@ -14,11 +14,13 @@ final class MainViewModel {
     
     let instrumentSelector: InstrumentSelectorViewModel
     let sampleControl: SampleControlViewModel
+    let controlPanel: ControlPanelViewModel
 
     init(activeLayer: AnyPublisher<Layer?, Never>) {
         
         self.instrumentSelector = .initial
         self.sampleControl = SampleControlViewModel(update: activeLayer.compactMap{ $0?.control }.eraseToAnyPublisher())
+        self.controlPanel = .initial
     }
 }
 
@@ -44,6 +46,28 @@ final class MainViewModelTests: XCTestCase {
         let sut = MainViewModel(activeLayer: Empty().eraseToAnyPublisher())
 
         XCTAssertNil(sut.sampleControl.control)
+    }
+    
+    func test_init_controlPanelContainsCorrectButtons() {
+        
+        let sut = MainViewModel(activeLayer: Empty().eraseToAnyPublisher())
+        
+        XCTAssertEqual(sut.controlPanel.layersButton.name, "Слои")
+        XCTAssertEqual(sut.controlPanel.layersButton.isActive, false)
+        XCTAssertEqual(sut.controlPanel.layersButton.isEnabled, true)
+        
+        XCTAssertEqual(sut.controlPanel.recordButton.type, .record)
+        XCTAssertEqual(sut.controlPanel.layersButton.isActive, false)
+        XCTAssertEqual(sut.controlPanel.layersButton.isEnabled, true)
+        
+        XCTAssertEqual(sut.controlPanel.composeButton.type, .compose)
+        XCTAssertEqual(sut.controlPanel.layersButton.isActive, false)
+        XCTAssertEqual(sut.controlPanel.layersButton.isEnabled, true)
+        
+        XCTAssertEqual(sut.controlPanel.playButton.type, .play)
+        XCTAssertEqual(sut.controlPanel.layersButton.isActive, false)
+        XCTAssertEqual(sut.controlPanel.layersButton.isEnabled, true)
+
     }
 
 }
