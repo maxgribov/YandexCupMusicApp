a5e5a015b0454ce30d38cc614e94a249e1c9279a
Max Gribov <gribov.max@gmail.com>
2023-11-25 17:55:32 +0300

    play result nothing playing on player node creation failure

--- Modules/Tests/ProcessingTests/AudioEnginePlayerTests.swift
+++ Modules/Tests/ProcessingTests/AudioEnginePlayerTests.swift
@@ -11,16 +11,42 @@ import Domain
 final class AudioEnginePlayer {
     
     var playing: Set<Layer.ID> { [] }
+    
+    init(makePlayerNode: @escaping (Data) -> AudioEnginePlayerNodeProtocol?) {}
+    
+    func play(id: Layer.ID, data: Data, control: Layer.Control) {}
+}
+
+protocol AudioEnginePlayerNodeProtocol {
+    
+    init?(with data: Data)
 }
 
 final class AudioEnginePlayerTests: XCTestCase {
-
     
     func test_init_nothingPlaying() {
         
-        let sut = AudioEnginePlayer()
+        let sut = AudioEnginePlayer(makePlayerNode: { _ in return nil })
+        
+        XCTAssertTrue(sut.playing.isEmpty)
+    }
+    
+    func test_play_nothingPlayingOnNodeCreationFailure() {
+        
+        let sut = AudioEnginePlayer(makePlayerNode: { data in
+            AlwaysFailingAudioEnginePlayerNodeStub(with: data)
+        })
         
         XCTAssertTrue(sut.playing.isEmpty)
     }
 
+    //MARK: - Helpers
+    
+    class AlwaysFailingAudioEnginePlayerNodeStub: AudioEnginePlayerNodeProtocol {
+        
+        required init?(with data: Data) {
+            
+            return nil
+        }
+    }
 }
