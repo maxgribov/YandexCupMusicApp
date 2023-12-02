e55a5ae1ee2a150a48b310a37a380dfbe95bd6a6
Max Gribov <gribov.max@gmail.com>
2023-12-02 11:29:04 +0600

    backButtonDidTapped messages delegate to dismiss

--- Modules/Tests/PresentationTests/VisualPlayerViewModelTests.swift
+++ Modules/Tests/PresentationTests/VisualPlayerViewModelTests.swift
@@ -12,9 +12,25 @@ import Domain
 final class VisualPlayerViewModel: ObservableObject {
     
     @Published private(set) var title: String
+    private let delegateActionSubject = PassthroughSubject<DelegateAction, Never>()
+    
+    enum DelegateAction: Equatable {
+        
+        case dismiss
+    }
+    
+    var delegateAction: AnyPublisher<DelegateAction, Never> {
+        
+        delegateActionSubject.eraseToAnyPublisher()
+    }
     
     init(title: String) {
         self.title = title
+    }
+    
+    func backButtonDidTapped() {
+        
+        delegateActionSubject.send(.dismiss)
     }
 
 }
@@ -26,5 +42,15 @@ final class VisualPlayerViewModelTests: XCTestCase {
         let sut = VisualPlayerViewModel(title: "track name")
         
         XCTAssertEqual(sut.title, "track name")
+    }
+    
+    func test_backButtonDidTap_messgesDelegateToDissmiss() {
+        
+        let sut = VisualPlayerViewModel(title: "track name")
+        let delegateActionSpy = ValueSpy(sut.delegateAction)
+        
+        sut.backButtonDidTapped()
+        
+        XCTAssertEqual(delegateActionSpy.values, [.dismiss])
     }
 }
